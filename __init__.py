61bf80f8b96b36dd13072ee78cdc4c9edbd4c517
WDgachtout <wdgachtout@gmail.com>
2025-01-14 09:29:00 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -32,7 +32,7 @@ def svg():
     return render_template('svg.html')
 
 @app.route('/maison')
-def svg():
+def maison():
     return render_template('maison.html')
     
 @app.route('/encrypt/<string:valeur>')
